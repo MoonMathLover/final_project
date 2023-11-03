3b2cddeb55c3e99f197de87c9564583cd7483131
alex0207s <alex640702070829@gmail.com>
2023-11-04 00:34:02 +0800

    :bug: fix an overscroll bouncing && refactor code

--- zkp-nft-dapp/src/components/Card.tsx
+++ zkp-nft-dapp/src/components/Card.tsx
@@ -5,37 +5,35 @@ const Card: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center pt-40">
-      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-700 dark:border-gray-700">
-        <a href="#">
-          <img
-            className="p-8 rounded-t-lg"
-            src="images/logo.png"
-            alt="logo image"
-          />
-        </a>
+    <div className="flex justify-center pt-40 pb-10">
+      <div className="max-w-sm border rounded-lg shadow-2xl bg-gray-700 border-gray-700">
+        <img
+          className="p-8 rounded-t-lg"
+          src="images/logo.png"
+          alt="logo image"
+        />
         <div className="px-5 pb-5">
-          <div className="text-3xl font-semibold tracking-tight text-gray-900 text-white mb-5">
+          <div className="text-2xl font-mono text-white p-3">
             Moon Math Lover NFT
           </div>
-          <div className="pb-5 font-mono text-gray-900 text-white">
+          <div className="pb-5 font-mono">
             <p className="text-sm text-gray-400">CONTRACT ADDRESS</p>
-            <p className="text-lg">{getShortAddress(testAddress)}</p>
+            <p className="text-lg text-white">{getShortAddress(testAddress)}</p>
           </div>
-          <div className="pb-5 font-mono text-gray-900 text-white">
+          <div className="pb-5 font-mono">
             <p className="text-sm text-gray-400">TOKEN STANDARD</p>
-            <p className="text-lg">ERC721</p>
+            <p className="text-lg text-white">ERC721</p>
           </div>
           <div className="flex items-center justify-between">
             <div className="flex flex-col">
-              <span className="text-sm text-slate-300">PRICE</span>
-              <span className="text-2xl font-bold text-gray-900 dark:text-white">
+              <span className="text-lg text-gray-400">PRICE</span>
+              <span className="text-2xl font-bold text-gray-900 text-white">
                 0.01 ETH
               </span>
             </div>
             <button
               type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-lg px-5 py-2.5 mr-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 rounded-lg text-xl px-5 py-2.5 m-3"
             >
               BUY IT
             </button>
