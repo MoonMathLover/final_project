e52771298537fd7f82c02c458c23ae7efdaf97e2
alex0207s <alex640702070829@gmail.com>
2023-11-06 02:39:59 +0800

    :bug: fix the header width

--- zkp-nft-dapp/src/components/Header.tsx
+++ zkp-nft-dapp/src/components/Header.tsx
@@ -5,7 +5,7 @@ import Logo from "./Logo";
 const Header = () => {
   return (
     <div className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
+      <div className="flex flex-wrap items-center justify-between p-4">
         <Logo />
         <div className="px-4 py-2 mr-3 md:mr-0 order-3 md:order-2">
           <ConnectWallet
