64bd1ce3a3a3928bd79e612f79ec38491256d956
alex0207s <alex640702070829@gmail.com>
2023-11-06 01:37:06 +0800

    :sparkles: add table skeleton

--- zkp-nft-dapp/src/pages/verifyPage/index.tsx
+++ zkp-nft-dapp/src/pages/verifyPage/index.tsx
@@ -1,9 +1,11 @@
 import ProgressStepper from "./components/ProgressStepper";
+import Table from "./components/Table";
 
 const Verify: React.FC = () => {
   return (
     <>
       <ProgressStepper />
+      <Table />
     </>
   );
 };
