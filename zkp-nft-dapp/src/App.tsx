1bce9463013ae678b9edb2000dfea4d96f54e4f4
alex0207s <alex640702070829@gmail.com>
2023-11-05 22:28:56 +0800

    add verify page skeleton

--- zkp-nft-dapp/src/App.tsx
+++ zkp-nft-dapp/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Card from "./components/Card";
+import Verify from "./pages/verifyPage";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Card />
+      <Verify />
     </>
   );
 }
